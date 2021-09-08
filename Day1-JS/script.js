7e77e95aa5206a162f199e4425a58e82abaaef79
S-NANTHAKUMAR-github-io <87605162+S-NANTHAKUMAR-github-io@users.noreply.github.com>
2021-09-08 11:52:08 +0530

    Add files via upload

---
+++ Day1-JS/script.js
@@ -0,0 +1,9 @@
+var one = 50;
+var two = 100;
+
+console.log(one + two);
+console.log(one - two);
+console.log(one * two);
+console.log(one / two);
+console.log(one % two);
+console.log(one ** two);
